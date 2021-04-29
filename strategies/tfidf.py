4e1436b29ab526c8888ad021820790961f0d4eb9
Vinícius Garcia <vingarcia00@gmail.com>
2021-04-29 12:15:09 -0300

    Improve tfidf strategy by adding stopwords and 2-grams

--- strategies/tfidf.py
+++ strategies/tfidf.py
@@ -2,10 +2,13 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer as v
 
+stopwords = [word.strip() for word in open('stopwords').readlines()]
+
 def keywordExtraction(docs, globalKeywords, minTokens=2, defaultKeywordScore=0, factor=1):
-    tfidf = v(use_idf=True)
+    tfidf = v(use_idf=True, stop_words='english')
 
     dataset = ['%s\n%s' % (doc['title'], doc['abstract']) for doc in docs]
+    tfidf.ngram_range = (2,2)
     model = tfidf.fit_transform(dataset)
     df = pd.DataFrame(model[0].T.todense(), index=tfidf.get_feature_names(), columns=["TF-IDF"])
     keywordsDict = df.to_dict()['TF-IDF']
